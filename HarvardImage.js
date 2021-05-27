1e7e868b298045378c45725e11b37c18c8f3fbe1
Skyler Aschenbeck <Skyler.Aschenbeck@improving.com>
2021-05-27 10:07:25 -0500

    assembling page over functionality

--- HarvardImage.js
+++ HarvardImage.js
@@ -1,26 +1,38 @@
 const input = document.querySelector("#query");
 const imageContainer = document.querySelector("#imageGrid");
 
+function produceImages(data){
+    data.records.forEach(element => {
+        const imageDiv = document.createElement("div");
+        imageDiv.classList.add("imageContainer");
+
+        const image = document.createElement("img");
+        image.classList.add("art");
+        image.src = element.baseimageurl;
+        image.alt = element.alttext;
+
+        const description = document.createElement("div");
+        description.classList.add("description");
+        description.textContent = element.description;
+        if(description.textContent == ""){
+            description.textContent = element.caption;
+        }
+
+        imageDiv.appendChild(image);
+        imageDiv.appendChild(description);
+        imageContainer.appendChild(imageDiv);
+    })
+}
 
 function imageFetch(inputQuery){
-    var apiEndpointBaseUrl ="https://api.harvardartmuseums.org/image?q=" + inputQuery + "&apikey=" + HarvardAPIKey;
+    let apiEndpointBaseUrl ="https://api.harvardartmuseums.org/image?q=" + inputQuery + "&apikey=" + HarvardAPIKey;
     fetch(apiEndpointBaseUrl)
     .then((repsonse) => {
         return repsonse.json();
     })
     .then((data) => {
         console.log(data);
-        data.records.forEach(element => {
-            const imageDiv = document.createElement("div");
-            imageDiv.classList.add("imageContainer")
-
-            const image = document.createElement("img");
-            image.classList.add("art");
-            image.src = element.baseimageurl;
-            
-            imageDiv.appendChild(image);
-            imageContainer.appendChild(imageDiv);
-        });
+        produceImages(data);
     })
 }
 
@@ -32,3 +44,4 @@ input.addEventListener('keyup', (e) => {
         imageFetch(query);
     }
 })
+
