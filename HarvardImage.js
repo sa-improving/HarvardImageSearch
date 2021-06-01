107f0c79b23952b22b7310e6c6e36ceb0602524a
Skyler Aschenbeck <Skyler.Aschenbeck@improving.com>
2021-05-31 23:58:13 -0500

    Updating paging, next sucessful, previous is WIP

--- HarvardImage.js
+++ HarvardImage.js
@@ -1,5 +1,9 @@
 const input = document.querySelector("#query");
 const imageContainer = document.querySelector("#imageGrid");
+const next = document.querySelector("#next");
+const previous = document.querySelector("#previous");
+let urlNext;
+let urlPrevious;
 
 function produceImages(data){
     data.records.forEach(element => {
@@ -33,8 +37,45 @@ function imageFetch(inputQuery){
     .then((data) => {
         console.log(data);
         produceImages(data);
+        if(data.info.next !== null){
+            urlNext = data.info.next;
+        } else {
+            urlNext = apiEndpointBaseUrl;
+        }
     })
 }
+
+function paging(url, direction){
+    let apiEndpointBaseUrl = url;
+    fetch(apiEndpointBaseUrl)
+    .then((repsonse) => {
+        return repsonse.json();
+    })
+    .then((data) => {
+        console.log(data);
+        let next = data.info.next;
+        let previous = data.info.prev;
+        if(direction === "next"){
+            if(next !== undefined){
+                imageContainer.textContent = ""
+                produceImages(data);
+                urlNext = next;
+            }
+            if(previous !== undefined){
+                urlPrevious = previous; 
+            }
+        }else{
+            if(previous !== undefined){
+                imageContainer.textContent = ""
+                produceImages(data);
+                urlPrevious = previous;
+            }
+            if(previous !== undefined){
+                urlNext = next; 
+            }
+        }
+    })
+} 
 
 input.addEventListener('keyup', (e) => {
     if(e.keyCode === 13){
@@ -45,3 +86,10 @@ input.addEventListener('keyup', (e) => {
     }
 })
 
+next.addEventListener('click', (currentPage) =>{
+    paging(urlNext, "next")
+})
+
+previous.addEventListener('click', (currentPage) =>{
+    paging(urlPrevious, "previous");
+})
